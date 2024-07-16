5e13fc4f2b0bce608fc8f071ab5c305c9618db2b
Tomas Vonracek <tomasvondrac@gmail.com>
2024-07-16 15:51:28 +0200

    test: concurrent run for react and vue e2e tests

--- packages/vue/cypress.config.js
+++ packages/vue/cypress.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
     e2e: {
         // This PORT must be in sync with the port defined in package.json
-        baseUrl: 'http://localhost:8000'
+        baseUrl: 'http://localhost:8002'
     }
 });
